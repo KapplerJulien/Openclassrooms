a047ffa2f8faa47cf6597e1bde6b960da6ec0658
xibougta <kappler.julien@gmail.com>
2020-09-29 19:11:44 +0200

    Commit : Afficher les articles

--- config/router.php
+++ config/router.php
@@ -37,8 +37,26 @@ class Router
                     $this->frontController->deconnexion();
                 } elseif($route === 'post'){
                     $this->frontController->post();
+                } elseif($route === 'auteur'){
+                    $this->frontController->auteur();
+                } elseif($route === 'contact'){
+                    $this->frontController->contact();
+                } elseif($route === 'article'){
+                    $this->frontController->article($this->request->getGet()->get('articleId'));
+                } elseif($route === 'nouvelArticle'){
+                    $this->frontController->nouvelArticle();
+                } elseif($route === 'ajoutArticle'){
+                    $this->frontController->ajoutArticle($this->request->getPost());
+                } elseif($route === 'suppressionArticle'){
+                    $this->frontController->suppressionArticle($this->request->getGet()->get('articleId'));
+                } elseif($route === 'pageModifArticle'){
+                    // var_dump($this->request->getGet()->get('articleId'));
+                    $this->frontController->pageModifArticle($this->request->getGet()->get('articleId'));
+                } elseif($route === 'modifArticle'){
+                    $this->frontController->modifArticle($this->request->getPost(), $this->request->getGet()->get('articleId'));
+                } elseif($route === 'commentaire'){
+                    $this->frontController->commentaireArticle($this->request->getPost(), $this->request->getGet()->get('articleId'));
                 } else {
-                    // $this->frontController->home();
                     $this->errorController->errorNotFound();
                 }
             }
@@ -48,11 +66,8 @@ class Router
         }
         catch (Exception $e)
         {
-            // var_dump($route);
-            // var_dump('errorServer');
             // var_dump($_SESSION);
             $this->errorController->errorServer();
-            // $this->frontController->home();
         }
     }
 }
--- src/DAO/PostDAO.php
+++ src/DAO/PostDAO.php
@@ -21,7 +21,7 @@ class PostDAO extends DAO
     {
         $db = new DAO();
         $connexion = $db->getConnection();
-        $sql = 'SELECT IdPost, NomPost, ContenuPost, AuteurPost, DateCreationPost FROM post ORDER BY IdPost DESC';
+        $sql = 'SELECT IdPost, NomPost, ContenuPost, AuteurPost, DateCreationPost FROM post ORDER BY IdPost DESC;';
         $result = $connexion->query($sql);
         $articles = [];
         foreach ($result as $row){
@@ -34,7 +34,86 @@ class PostDAO extends DAO
 
     public function getArticle($articleId)
     {
-        $sql = 'SELECT id, title, content, author, createdAt FROM article WHERE id = ?';
-        return $this->createQuery($sql, [$articleId]);
+        $db = new DAO();
+        $connexion = $db->getConnection();
+        $sql = 'SELECT IdPost, NomPost, ContenuPost, AuteurPost, DateCreationPost FROM post WHERE idPost ='.$articleId.';';
+        // var_dump($sql);
+        $result = $connexion->query($sql);
+        $article = [];
+        foreach ($result as $row){
+            $articleId = $row['IdPost'];
+            $article[$articleId] = $this->buildObject($row);
+        }
+        $result->closeCursor();
+        return $article;
     }
+
+    public function getArticleAuteur($idAuteur)
+    {
+        $db = new DAO();
+        $connexion = $db->getConnection();
+        $sql = 'SELECT IdPost, NomPost, ContenuPost, AuteurPost, DateCreationPost FROM post where IdUtilisateur = '.$idAuteur.' ORDER BY IdPost DESC;';
+        $result = $connexion->query($sql);
+        $articles = [];
+        foreach ($result as $row){
+            $articleId = $row['IdPost'];
+            $articles[$articleId] = $this->buildObject($row);
+        }
+        $result->closeCursor();
+        return $articles;
+    }
+
+    public function ajouterArticle($post, $idAuteur){
+        $db = new DAO();
+        $connexion = $db->getConnection();
+        $sqlAuteurPost = 'select NomUtilisateur from utilisateur where IdUtilisateur ='.$idAuteur.';';
+        $dataAuteurPost = $connexion->query($sqlAuteurPost);
+        $auteurPost = $dataAuteurPost->fetch();
+        // var_dump($auteurPost);
+        $date = date('Y-m-d');
+        $sql = 'INSERT INTO post ( NomPost, ChapoPost, ContenuPost, AuteurPost, DateCreationPost, DateDerniereModifPost, IdUtilisateur) 
+        VALUES ("'.$post->get('titre').'","'.$post->get('chapo').'","'.$post->get('contenu').'","'.$auteurPost['NomUtilisateur'].'","'.$date.'","'.$date.'",'.$idAuteur.');';
+        // var_dump($sql);
+        $data = $connexion->query($sql);
+    }
+
+    public function suppressionArticle($articleId){
+        $db = new DAO();
+        $connexion = $db->getConnection();
+        $sql = 'delete from post where IdPost = '.$articleId.';';
+        $data = $connexion->query($sql);
+    }
+
+    public function modifArticle($post, $articleId){
+        $db = new DAO();
+        $connexion = $db->getConnection();
+        $date = date('Y-m-d');
+        $sql = 'update post set NomPost = "'.$post->get('titre').'", ChapoPost = "'.$post->get('chapo').'", ContenuPost = "'.$post->get('contenu').'", AuteurPost = "'.$post->get('auteur').'", DateDerniereModifPost = "'.$date.'"
+        where IdPost = '.$articleId.';';
+        $data = $connexion->query($sql);
+    }
+
+    public function getArticleModifAuteur($articleId){
+        $db = new DAO();
+        $connexion = $db->getConnection();
+        $sql = 'SELECT IdPost, NomPost, ChapoPost, ContenuPost, AuteurPost FROM post where IdPost = '.$articleId.';';
+        // var_dump($sql);
+        $data = $connexion->query($sql);
+        $result = $data->fetch();
+        $data->closeCursor();
+        return $result;
+    }
+
+    public function addCom($post, $articleId, $idUser){
+        // var_dump($post->get('message'));
+        $db = new DAO();
+        $connexion = $db->getConnection();
+        $date = date('Y-m-d');
+        $sql = 'INSERT INTO commentaire (ContenuCommentaire, DateCommentaire, IdEtatCommentaire, IdUtilisateur, IdPost) 
+        VALUES ("'.$post->get('message').'","'.$date.'",2,'.$idUser.','.$articleId.');';
+        // var_dump($sql);
+        $data = $connexion->query($sql);
+    }
+
+    
 }
